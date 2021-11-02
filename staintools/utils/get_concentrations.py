fea95454ef73b966298ac05eb3f007f6504cb40d
Christian Marzahl <christian.marzahl@gmail.com>
2021-11-02 10:43:35 +0100

    Try NumThreads = 0

--- staintools/utils/get_concentrations.py
+++ staintools/utils/get_concentrations.py
@@ -1,5 +1,5 @@
-#import spams
-from sklearn.linear_model import Lasso
+import spams
+#from sklearn.linear_model import Lasso
 
 from staintools.utils.optical_density_conversion import convert_RGB_to_OD
 
@@ -14,7 +14,7 @@ def get_concentrations(I, stain_matrix, regularizer=0.01):
     :return:
     """
     OD = convert_RGB_to_OD(I).reshape((-1, 3))
-    #return spams.lasso(X=OD.T, D=stain_matrix.T, mode=2, lambda1=regularizer, pos=True, numThreads=1).toarray().T
+    return spams.lasso(X=OD.T, D=stain_matrix.T, mode=2, lambda1=regularizer, pos=True, numThreads=0).toarray().T
     
-    lasso = Lasso(alpha=regularizer, positive=True)
-    return lasso.fit(stain_matrix.T, OD.T)
+    #lasso = Lasso(alpha=regularizer, positive=True)
+    #return lasso.fit(stain_matrix.T, OD.T)
